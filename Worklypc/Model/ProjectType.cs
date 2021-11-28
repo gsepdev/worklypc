1909cd8bd40a2ba69f4e8778312dc1d8c6f6624c
Goldi Sepulveda <goldis@peacocks.com.au>
2021-11-29 10:19:38 +1100

    first commit

---
+++ Worklypc/Model/ProjectType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Worklypc.Model
+{
+    public class ProjectType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+
+    }
+}
